82c45fa09a712279f17ded1db33e849e877319e5
Georgi <go6e03@gmail.com>
2025-10-05 19:25:29 +0300

    Updated files with license

--- main/ws_comms.h
+++ main/ws_comms.h
@@ -1,5 +1,23 @@
-#ifndef __SEND_DATA_PACK
-#define __SEND_DATA_PACK
+/* 
+ * ESP32 Corsa Fuel Meter
+ * Copyright (C) 2025 Georgi Georgiev
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef __WS_COMMS_H
+#define __WS_COMMS_H
 
 #include "websocket.h"
 #include "esp_log.h"
